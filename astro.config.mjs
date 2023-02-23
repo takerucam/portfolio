64b25266b50e053a31e6b3009ab572785d923628
Takeru Miyake <m18kecam777@gmail.com>
2023-02-23 20:23:42 +0900

    🚑 renderが見つからない不具合修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config'
 import cloudflare from '@astrojs/cloudflare'
+import node from '@astrojs/node'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
@@ -13,6 +14,9 @@ export default defineConfig({
   output: 'server',
   adapter: cloudflare({
     mode: 'advanced',
+  }),
+  adapter: node({
+    mode: 'standalone',
   }),
   integrations: [tailwind(), preact()],
 })
